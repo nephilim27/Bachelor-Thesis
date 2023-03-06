29aea0a128114fab4a1044f09f8dd397419c28da
Tovisi Zoltan <tovisi.zoltan@student.ms.sapientia.ro>
2023-03-06 16:07:39 +0200

    initial commit

---
+++ backend/src/main/java/com/example/final_exam_backend/FinalExamBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.final_exam_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalExamBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalExamBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/final_exam_backend/FinalExamBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.final_exam_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalExamBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
