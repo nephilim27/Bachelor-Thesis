872cbdc39bc3a33beebed67568ba364fe8574c1b
Tovisi Zoltan <tovisi.zoltan@student.ms.sapientia.ro>
2023-03-15 16:50:38 +0200

    Implemented Google authentification

--- backend/src/main/java/com/example/final_exam_backend/FinalExamBackendApplication.java
+++ backend/src/main/java/com/example/final_exam_backend/FinalExamBackendApplication.java
@@ -2,8 +2,9 @@ package com.example.final_exam_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class FinalExamBackendApplication {
 
 	public static void main(String[] args) {
